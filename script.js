3196a58591bdd551e32536f193398e5dd5a9e1e2
isaacGTeles <isaac.gabriel1999@gmail.com>
2025-01-08 09:59:15 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 function isPalindrome(str){
-    /* Seu código aqui */
+    
+    str = str.toLowerCase();
+    str = str.replace(/[\W_]+/g, '');
+
+    for(let i = 0; i < Math.floor(str.length / 2); i++){
+        if(str[i] !== str[str.length - 1 - i]){
+            return false;
+        }
+    }
+
+    return true;
 }
 
 function arrayMaxMin(arr){
-    /* Seu código aqui */
+    let min = 0, max = 0;
+    for(let i = 0; i < arr.length; i++){
+        if(i === 0){
+            min = arr[i];
+            max = arr[i];
+        }else{
+            if(arr[i] < min){
+                min = arr[i];
+            }else if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+    }
+    return [min, max];
 }
